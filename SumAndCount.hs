75ebb2ae78b183c01cf97c819baee5b58c54ebf5
Ilya Burinsky <burinskiy992@gmail.com>
2023-05-25 20:30:09 +0300

    SumAndCount.hs

---
+++ SumAndCount.hs
@@ -0,0 +1,8 @@
+sum'n'count :: Integer -> (Integer, Integer)
+sum'n'count x
+  | x == 0 = (0, 1)
+  | otherwise = let
+    helper num digitsSum digitsCnt
+      | num == 0 = (digitsSum, digitsCnt)
+      | otherwise = helper (num `div` 10 ) (digitsSum + num `mod` 10) (digitsCnt + 1)
+    in helper (abs (-x)) 0 0
