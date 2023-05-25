6053d5853601feb41d5a2fac8763dfbf37f9e970
Ilya Burinsky <burinskiy992@gmail.com>
2023-05-25 20:29:22 +0300

    SeqA.hs

---
+++ SeqA.hs
@@ -0,0 +1,9 @@
+seqA :: Integer -> Integer
+
+seqA n = let
+  helper a b c n
+    | n == 0 = a
+    | n == 1 = b
+    | n == 2 = c
+    | otherwise = helper b c (b + c - 2 * a) (n - 1)
+  in helper 1 2 3 n
