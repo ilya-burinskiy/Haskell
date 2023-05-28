c449e400259630e259fb99261d674b57d58760df
Ilya Burinsky <burinskiy992@gmail.com>
2023-05-28 18:29:35 +0300

    made Cmps Foldable and Traversable

--- Cmps.hs
+++ Cmps.hs
@@ -8,7 +8,14 @@ instance (Functor f, Functor g) => Functor (f |.| g) where
 
 instance (Applicative f, Applicative g) => Applicative (f |.| g) where
   pure = Cmps . pure . pure
-  (Cmps h) <*> (Cmps x) = Cmps $ fmap (<*>) h <*> x 
+  (Cmps h) <*> (Cmps x) = Cmps $ fmap (<*>) h <*> x
+
+instance (Foldable f, Foldable g) => Foldable (f |.| g) where
+  foldr :: (Foldable f, Foldable g) => (a -> b -> b) -> b -> (|.|) f g a -> b
+  foldr f ini (Cmps composition) = let h cmps ini = foldr f ini cmps in foldr h ini composition
+
+instance (Traversable f, Traversable g) => Traversable (f |.| g) where
+  traverse f (Cmps x) = Cmps <$> traverse (traverse f) x
 
 unCmps3 :: Functor f => (f |.| g |.| h) a -> f (g (h a))
 unCmps3 (Cmps x) = fmap getCmps x
--- FoldableCmps.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE TypeOperators #-}
-
-infixr |.|
-newtype (|.|) f g a = Cmps { getCmps :: f (g a) } deriving (Eq, Show)
-
-instance (Foldable f, Foldable g) => Foldable (f |.| g) where
-  foldr :: (Foldable f, Foldable g) => (a -> b -> b) -> b -> (|.|) f g a -> b
-  foldr f ini (Cmps composition) = let h cmps ini = foldr f ini cmps in foldr h ini composition
