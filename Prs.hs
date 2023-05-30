6c1032916935fda6d980e1053b238c070f61a20b
Ilya Burinsky <burinskiy992@gmail.com>
2023-05-30 14:33:29 +0300

    nat parser

--- Prs.hs
+++ Prs.hs
@@ -2,6 +2,7 @@
 
 import Data.Text.Internal.Read (IParser(runP))
 import Control.Applicative (Alternative, empty, (<|>))
+import Data.Char (isDigit)
 
 newtype Prs a = Prs { runPrs :: String -> Maybe (a, String) }
 
@@ -24,8 +25,26 @@ instance Alternative Prs where
       Just (val, strRem) -> Just (val, strRem)
       Nothing            -> runPrs p2 str
 
-
 anyChr :: Prs Char
 anyChr = Prs $ \case
     char : tail -> Just (char, tail)
     _           -> Nothing
+
+digit = Prs $ \ case
+  char : chars -> if isDigit char then Just (char, chars) else Nothing
+  _ -> Nothing
+
+many :: Prs a -> Prs [a]
+many p = (:) <$> p <*> many p <|> pure []
+
+many1 :: Prs a -> Prs [a]
+many1 p = Prs $ \ str -> do
+  (x, strRem) <- runPrs p str
+  (xs, strRem') <- runPrs (many p) strRem
+  return (x : xs, strRem')
+
+nat :: Prs Int
+nat = Prs $ \ str ->
+  case runPrs (many1 digit) str of
+    Just (num, str') -> Just (read num :: Int, str')
+    Nothing          -> Nothing
