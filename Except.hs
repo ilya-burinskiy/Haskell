0e0a6e4bf8e19a24a47309c2206576cbd46270e4
Ilya Burinsky <burinskiy992@gmail.com>
2023-05-31 14:19:59 +0300

    moved ListIndexError to Except.hs

--- Except.hs
+++ Except.hs
@@ -4,6 +4,17 @@ import Control.Applicative (Alternative(empty, (<|>)))
 newtype Except e a = Except { runExcept :: Either e a } deriving (Eq, Show)
 data ReadError = EmptyInput | NoParse String deriving Show
 data SumError = SumError Int ReadError deriving Show
+data ListIndexError = ErrIndexTooLarge Int | ErrNegativeIndex
+  deriving (Eq, Show)
+
+(!!!) :: [a] -> Int -> Except ListIndexError a
+(!!!) list i | i < 0 = throwE ErrNegativeIndex
+             | otherwise =
+  let
+    iter idx (x : xs) | idx < i  = iter (idx + 1) xs
+                      | idx == i = return x
+    iter idx [] = throwE $ ErrIndexTooLarge i
+  in iter 0 list
 
 instance Functor (Except e) where
   fmap = liftM
