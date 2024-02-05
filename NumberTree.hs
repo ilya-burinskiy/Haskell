a05bda97a382a43bf1d151f773aeb2dad284e831
Ilya Burinsky <burinskiy992@gmail.com>
2024-02-05 16:52:43 +0300

    numberAndCount

--- NumberTree.hs
+++ NumberTree.hs
@@ -1,27 +1,32 @@
-import Control.Monad
+import Control.Monad.RWS (Sum (Sum, getSum))
+import Control.Monad.Trans (lift)
+import Control.Monad.Trans.State.Lazy (State, StateT, evalState, evalStateT, get, modify, put, runState, state)
+import Control.Monad.Trans.Writer (Writer, runWriter, tell)
 
-data Tree a = Leaf a | Fork (Tree a) a (Tree a)
-newtype State s a = State { runState :: s -> (a, s) }
-
-instance Functor (State s) where
-  fmap = liftM
-
-instance Applicative (State s) where
-  pure = return
-  (<*>) = ap
-
-instance Monad (State s) where
-  p >>= k = State $ \ s0 ->
-   let (x, s1) = runState p s0
-   in runState (k x) s1
-
-numberTree' :: Tree () -> State Integer (Tree Integer)
-
-numberTree' (Leaf _) = State $ \num -> (Leaf num, num + 1)
-numberTree' (Fork left _ right) = State $ \num ->
-  let (numberedLeft, rootNum) = runState (numberTree' left) num
-      (numberedRight, nextNodeNum) = runState (numberTree' right) (rootNum + 1)
-  in (Fork numberedLeft rootNum numberedRight, nextNodeNum)
+data Tree a = Leaf a | Fork (Tree a) a (Tree a) deriving (Show)
 
 numberTree :: Tree () -> Tree Integer
-numberTree tree = fst $ runState (numberTree' tree) 1
+numberTree tree = evalState (go tree) 1
+  where
+    go :: Tree () -> State Integer (Tree Integer)
+    go (Leaf _) = state $ \num -> (Leaf num, num + 1)
+    go (Fork left _ right) = state $ \num ->
+      let (numberedLeft, rootNum) = runState (go left) num
+          (numberedRight, nextNodeNum) = runState (go right) (rootNum + 1)
+       in (Fork numberedLeft rootNum numberedRight, nextNodeNum)
+
+numberAndCount :: Tree () -> (Tree Integer, Integer)
+numberAndCount t = getSum <$> runWriter (evalStateT (go t) 1)
+  where
+    go :: Tree () -> StateT Integer (Writer (Sum Integer)) (Tree Integer)
+    go (Leaf _) = do
+      lift $ tell (Sum 1)
+      num <- get
+      modify (+ 1)
+      return (Leaf num)
+    go (Fork left _ right) = do
+      numberedLeft <- go left
+      num <- get
+      modify (+ 1)
+      numberedRight <- go right
+      return (Fork numberedLeft num numberedRight)
