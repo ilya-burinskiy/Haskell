8f0eb3722cf4bff08414a4a3339786501a6bfbc3
Ilya Burinsky <burinskiy992@gmail.com>
2024-02-08 19:54:40 +0300

    Checkpointed fixup

--- Checkpointed.hs
+++ Checkpointed.hs
@@ -1,30 +1,81 @@
-{-
-runCheckpointed p f = runCont (f $ \x -> Cont $ \c -> if p (c x) then c x else x) id
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-addTens' 1 checkpoint =
-  cont $ \ c1 ->
-    runCont
-      (checkpoint 1)
-      (\ _ ->
-        (\ c2 ->
-          runCont
-            (checkpoint 11)
-            (\ _ ->
-              (\ c3 ->
-                runCont
-                  (checkpoint 21)
-                  (\ _ -> (\ c -> c 31) c3)) c2)) c1)
+{-# HLINT ignore "Collapse lambdas" #-}
+{-# HLINT ignore "Use let" #-}
+{-# HLINT ignore "Redundant lambda" #-}
+{-# HLINT ignore "Redundant bracket" #-}
+{-# HLINT ignore "Monad law, left identity" #-}
+{-# HLINT ignore "Avoid lambda using `infix`" #-}
+newtype Cont r a = Cont {runCont :: (a -> r) -> r}
+
+instance Functor (Cont r) where
+  fmap :: (a -> b) -> Cont r a -> Cont r b
+  fmap f (Cont c') = Cont $ \c -> c' $ \a -> c (f a)
+
+instance Applicative (Cont r) where
+  pure :: a -> Cont r a
+  pure x = Cont $ \c -> c x
+  (<*>) :: Cont r (a -> b) -> Cont r a -> Cont r b
+  (<*>) (Cont f) (Cont k) = Cont $ \c -> k (\a -> f (\c' -> c (c' a)))
+
+instance Monad (Cont r) where
+  return :: a -> Cont r a
+  return = pure
+  (>>=) :: Cont r a -> (a -> Cont r b) -> Cont r b
+  (>>=) (Cont c) k = Cont $ \c' -> c (\a -> runCont (k a) c')
+
+sumIt :: Cont String Integer
+sumIt =
+  Cont (\c -> c 3) >>= \a ->
+    Cont (\c -> c 4 ++ c 5) >>= \b ->
+      return $ a + b
+
+x' :: (Integer -> [a]) -> [a]
+x' =
+  \c1 ->
+    (\c -> c 3)
+      ( \a1 ->
+          ( ( \a ->
+                \c2 ->
+                  (\c -> c 4 ++ c 5)
+                    (\a2 -> ((\b -> \c3 -> c3 $ a + b) a2) c2)
+            )
+              a1 -- 3
+          )
+            c1 -- show
+      )
+
+{-
+  самое внутренне отложенное вычислениe: \c3 -> c3 $ a + b (a и b связаны выше)
+  убираем абстрагирование по b: \c3 -> c3 $ a + a2 -- a2 это результат "среднего" вычисления
+  "среднее" вычисление: \c2 -> (\c -> c 4 ++ c 5) (\a2 -> (\c3 -> c3 $ a + a2) c2)
+  \c2 -> (\a2 -> (\c3 -> c3 $ a + a2) c2) 4 ++ (\a2 -> (\c3 -> c3 $ a + a2) c2) 5
+  \c2 -> (\c3 -> c3 $ a + 4) c2 ++ (\c3 -> c3 $ a + 5) c2
+
+  (\a -> \c2 -> (\c3 -> c3 $ a + 4) c2 ++ (\c3 -> c3 $ a + 5) c2)
+  убираем абстрагирование по a: \c2 -> (\c3 -> c3 $ a + 4) c2 ++ (\c3 -> c3 $ a1 + 5) c2
+  самое верхнее вычисление
+  \c1 -> (\c -> c 3) (\a1 -> (\c2 -> (\c3 -> c3 $ a + 4) c2 ++ (\c3 -> c3 $ a1 + 5) c2) c1)
+  \c1 -> (\a1 -> (\c2 -> (\c3 -> c3 $ a + 4) c2 ++ (\c3 -> c3 $ a1 + 5) c2) c1) 3
+  \c1 -> (\c2 -> (\c3 -> c3 $ 3 + 4) c2 ++ (\c3 -> c3 $ 3 + 5) c2) c1
 -}
 
-import Control.Monad.Cont
+bind :: ((a -> r) -> r) -> (a -> (b -> r) -> r) -> (b -> r) -> r
+bind v k = \c -> v (\a -> k a c)
+
+x'' = \c ->
+  (\c -> c 4 ++ c 5)
+    (\a -> (\b -> (\c -> c (3 + b))) a c)
+
+x''' = \c ->
+  (\b -> (\c -> c (3 + b))) 4 c ++ (\b -> (\c -> c (3 + b))) 5 c
+
+x = \c1 -> (\c2 -> (\c3 -> c3 $ 3 + 4) c2 ++ (\c3 -> c3 $ 3 + 5) c2) c1
+
 type Checkpointed a = (a -> Cont a a) -> Cont a a
 
-runCheckpointed :: (a -> Bool) -> Checkpointed a -> a
-runCheckpointed predicate checkpoint = runCont
-  (checkpoint $ \ x -> cont $ \ c -> if predicate (c x) then c x else x) id
-
 addTens :: Int -> Checkpointed Int
-addTens x1 checkpoint = do
+addTens x1 = \checkpoint -> do
   checkpoint x1
   let x2 = x1 + 10
   checkpoint x2
@@ -33,60 +84,84 @@ addTens x1 checkpoint = do
   let x4 = x3 + 10
   return x4
 
-addTens' x1 checkpoint =
-  cont $ \ c1 ->
-    runCont
-      (checkpoint x1)
-      (\ _ ->
-        (\ c2 ->
-          runCont
-            (checkpoint (x1 + 10))
-            (\ _ ->
-              (\ c3 ->
-                runCont
-                  (checkpoint (x1 + 20))
-                  (\ _ -> (\ c -> c (x1 + 30)) c3)) c2)) c1)
+runCheckpointed :: (a -> Bool) -> Checkpointed a -> a
+runCheckpointed pred checkpoint =
+  runCont
+    ( checkpoint
+        ( \x ->
+            Cont
+              ( \c ->
+                  let futureX = c x
+                   in if pred futureX
+                        then futureX
+                        else x
+              )
+        )
+    )
+    id
 
-checkpoint x =
-  let predicate = (< 30) in
-  if predicate x then cont $ \ c -> c x
-  else cont $ const x
+y :: Cont r Integer
+y = Cont $ \k ->
+  runCont
+    ( do
+        x <- return 10
+        y <- return (x * 2)
+        Cont $ \c -> k 5
+        return $ x + y
+    )
+    k
 
+y' :: Cont r Int
+y' = Cont $ \k ->
+  runCont
+    ( do
+        x <- return 10
+        y <- return (x * 2)
+        (\a -> Cont $ \c -> k a) 5
+        return $ x + y
+    )
+    k
+
+y'' :: Cont r Integer
+y'' = Cont $ \k ->
+  runCont
+    ( ( \exit ->
+          ( return 10 >>= \x ->
+              return (x * 2) >>= \y ->
+                exit 5 >>= \_ ->
+                  return $ x + y
+          )
+      )
+        (\a -> Cont $ \c -> k a)
+    )
+    k
+
+y''' :: Cont r Integer
+y''' =
+  (\f -> Cont $ \k -> runCont (f (\a -> Cont $ \c -> k a)) k)
+    ( \exit ->
+        ( return 10 >>= \x ->
+            return (x * 2) >>= \y ->
+              exit 5 >>= \_ ->
+                return $ x + y
+        )
+    )
+
+callCC :: ((a1 -> Cont r a2) -> Cont r a1) -> Cont r a1
+callCC f = Cont $ \k -> runCont (f (\a -> Cont $ \_ -> k a)) k
 
 {-
-s >>= f = cont $ \c -> runCont s $ \x -> runCont (f x) c
-
-sumIt = do
-  a <- return 3
-  b <- Cont $ \ c -> c 4 ++ c 5
-  return $ a + b
-
-sumIt =
-  (return 3) >>= (\ a -> do
-    b <- Cont $ \ c -> c 4 ++ c 5
-    return $ a + b)
-
-sumIt = return 3 >>= (\ a -> Cont $ \ c -> c 4 ++ c 5 >>= (\ b -> return $ a + b))
-sumIt = (Cont $ \ c -> c 3) >>= (\ a -> Cont $ \ c -> c 4 ++ c 5 >>= (\ b -> Cont $ \c -> c $ a + b))
-sumIt = Cont (\c -> (\ c -> c 3) (\ x -> runCont ((\ a -> Cont (\ c -> c 4 ++ c 5) >>= (\ b -> Cont $ \c -> c $ a + b)) x) c))
-sumIt = Cont (\c -> (\ c -> c 3) (\ x -> runCont ((\ a -> Cont (\ c -> (\ c -> c 4 ++ c 5) (\ x -> runCont ((\ b -> Cont $ \c -> c $ a + b) x) c))) x) c))
-runCont sumIt show
-(\c -> (\ c -> c 3) (\ x -> runCont ((\ a -> Cont (\ c -> (\ c -> c 4 ++ c 5) (\ x -> runCont ((\ b -> Cont $ \c -> c $ a + b) x) c))) x) c)) show
-(\c -> (\ c -> c 3) (\ x -> runCont ((\ a -> Cont (\ c -> (\ c -> c 4 ++ c 5) (\ x -> runCont (Cont $ \c -> c $ a + x) c))) x) c)) show
-(\c1 -> (\ c2 -> c2 3) (\ x1 -> runCont ((\ a -> Cont (\ c3 -> (\ c4 -> c4 4 ++ c4 5) (\ x2 -> runCont (Cont $ \c5 -> c5 $ a + x2) c3))) x1) c1)) show
-
-\ c3 -> (\ c4 -> c4 4 ++ c4 5) (\ x2 -> runCont (Cont $ \c5 -> c5 $ a + x2) c3)
-\ c3 -> (\ x2 -> runCont (Cont $ \c5 -> c5 $ a + x2) c3) 4 ++ (\ x2 -> runCont (Cont $ \c5 -> c5 $ a + x2) c3) 5
-\ c3 -> (runCont (Cont $ \c5 -> c5 $ a + 4) c3) ++ (runCont (Cont $ \c5 -> c5 $ a + 5) c3)
-\ c3 -> ((\c5 -> c5 $ a + 4) c3) ++ ((\c5 -> c5 $ a + 5) c3)
-\ c3 -> (c3 $ a + 4) ++ (c3 $ a + 5)
-
-(\c1 -> (\ c2 -> c2 3) (\ x1 -> runCont ((\ a -> Cont (\ c3 -> (c3 $ a + 4) ++ (c3 $ a + 5))) x1) c1)) show
-(\c1 -> (\ c2 -> c2 3) (\ x1 -> runCont (cont (\ c3 -> c3 (x1 + 4) ++ c3 (x1 + 5))) c1)) show
-(\c1 -> (\ c2 -> c2 3) (\ x1 -> ((\ c3 -> c3 (x1 + 4) ++ c3 (x1 + 5))) c1)) show
-(\c1 -> (\ c2 -> c2 3) (\ x1 -> (\ c3 -> c3 (x1 + 4) ++ c3 (x1 + 5)) c1)) show
-(\c1 -> (\ x1 -> c1 (x1 + 4) ++ c1 (x1 + 5)) 3) show
-(\c1 -> c1 (3 + 4) ++ c1 (3 + 5)) show
-show (3 + 4) ++ show (3 + 5)
-"78"
+( \exit ->
+          ( return 10 >>= \x ->
+              return (x * 2) >>= \y ->
+                exit 5 >>= \_ ->
+                  return $ x + y
+          )
+      )
+square n = callCC $ \k -> k (n*n)
+= Cont $ \k -> runCont ((\k -> k (n*n)) (\a -> Cont $ \_ -> k a)) k
+= Cont $ \k -> runCont ((\a -> Cont $ \_ -> k a) (n*n)) k
+= Cont $ \k -> runCont (Cont $ \_ -> k (n*n)) k
+= Cont $ \k -> (\_ -> k (n*n)) k
+= Cont $ \k -> k (n*n)
 -}
