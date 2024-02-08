3cef7bf6e94d5c14e0c4a6e396dda167a499a9ff
Ilya Burinsky <burinskiy992@gmail.com>
2024-02-08 19:54:00 +0300

    MyRWT

--- MyRWT.hs
+++ MyRWT.hs
@@ -1,6 +1,7 @@
 import Control.Monad.Reader
 import Control.Monad.Writer.Lazy
 import Data.Char (toUpper)
+import Data.List (partition)
 
 type MyRWT m = ReaderT [String] (WriterT String m)
 
@@ -24,3 +25,24 @@ logFirstAndRetSecond' = do
   myLift (putStrLn $ "Second is " ++ show el2)
   myTell el1
   return el1
+
+logFirstAndRetSecond'' :: MyRWT Maybe String
+logFirstAndRetSecond'' = do
+  xs <- myAsks id
+  case xs of
+    (x1 : x2 : _) -> myTell x1 >> return (map toUpper x2)
+    _ -> myLift Nothing
+
+-- myWithReader :: (Monad m) => (r' -> r) ->  MyRWT -> Reader r
+veryComplexComputation :: MyRWT Maybe (String, String)
+veryComplexComputation = do
+  xs <- myAsks id
+  let (evenLenStrs, oddLenStrs) = partition (\str -> even $ length str) xs
+  case evenLenStrs of
+    (x1 : x2 : _) -> do
+      case oddLenStrs of
+        (y1 : y2 : _) -> do
+          myTell $ x1 ++ "," ++ y1
+          return (map toUpper x2, map toUpper y2)
+        _ -> myLift Nothing
+    _ -> myLift Nothing
