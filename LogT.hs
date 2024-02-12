abcbc8d86278accd71d1ab2e6e35a35f43bb6c3c
Ilya Burinsky <burinskiy992@gmail.com>
2024-02-12 17:35:21 +0300

    updated LogT

--- LogT.hs
+++ LogT.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 import Control.Applicative (liftA2)
 import Control.Monad.Identity (Identity (runIdentity))
+import Control.Monad.Reader (ReaderT (ReaderT), runReaderT)
+import Control.Monad.Reader.Class (MonadReader, ask, local, reader)
+import Control.Monad.State.Class (MonadState, get, put, state)
 import Control.Monad.Trans.Class (MonadTrans, lift)
+import Control.Monad.Trans.State (State, StateT (StateT), modify, runState, runStateT)
+
+class (Monad m) => MonadLogg m where
+  w2log :: String -> m ()
+  logg :: Logged a -> m a
 
 data Logged a = Logged String a deriving (Eq, Show)
 
@@ -40,6 +52,49 @@ instance MonadTrans LoggT where
   lift :: (Monad m) => m a -> LoggT m a
   lift m = LoggT (fmap (Logged "") m)
 
+instance (MonadState s m) => MonadState s (LoggT m) where
+  get :: (MonadState s m) => LoggT m s
+  get = lift get
+
+  put :: (MonadState s m) => s -> LoggT m ()
+  put s = lift (put s)
+
+  state :: (MonadState s m) => (s -> (a, s)) -> LoggT m a
+  state f = lift (state f)
+
+mapLoggT :: (m (Logged a) -> n (Logged b)) -> LoggT m a -> LoggT n b
+mapLoggT f = LoggT . f . runLoggT
+
+instance (MonadReader r m) => MonadReader r (LoggT m) where
+  ask :: (MonadReader r m) => LoggT m r
+  ask = lift ask
+
+  local :: (MonadReader r m) => (r -> r) -> LoggT m a -> LoggT m a
+  local f = mapLoggT (local f)
+
+  reader :: (MonadReader r m) => (r -> a) -> LoggT m a
+  reader f = lift (reader f)
+
+instance (Monad m) => MonadLogg (LoggT m) where
+  w2log :: (Monad m) => String -> LoggT m ()
+  w2log = write2log
+
+  logg :: (Monad m) => Logged a -> LoggT m a
+  logg l = LoggT (return l)
+
+instance (MonadLogg m) => MonadLogg (StateT s m) where
+  w2log :: (MonadLogg m) => String -> StateT s m ()
+  w2log = lift . w2log
+  logg :: (MonadLogg m) => Logged a -> StateT s m a
+  logg = lift . logg
+
+instance (MonadLogg m) => MonadLogg (ReaderT r m) where
+  w2log :: (MonadLogg m) => String -> ReaderT r m ()
+  w2log = lift . w2log
+
+  logg :: (MonadLogg m) => Logged a -> ReaderT r m a
+  logg = lift . logg
+
 write2log :: (Monad m) => String -> LoggT m ()
 write2log str = LoggT (return (Logged str ()))
 
@@ -47,3 +102,24 @@ type Logg = LoggT Identity
 
 runLogg :: Logg a -> Logged a
 runLogg = runIdentity . runLoggT
+
+logSt'' :: LoggT (State Integer) Integer
+logSt'' = do
+  x <- logg $ Logged "BEGIN " 1
+  lift $ modify (+ x) -- не работает без lift, но тесты прошли
+  a <- get
+  w2log $ show $ a * 10
+  put 42
+  w2log " END"
+  return $ a * 100
+
+rdrStLog :: ReaderT Integer (StateT Integer Logg) Integer
+rdrStLog = do
+  x <- logg $ Logged "BEGIN " 1
+  y <- ask
+  lift $ modify (+ (x + y)) -- не работает без lift, но тесты прошли
+  a <- get
+  w2log $ show $ a * 10
+  put 42
+  w2log " END"
+  return $ a * 100
