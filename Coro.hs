33e3e6baf4fd12bc0ef213042bdeee8419553a82
ilya-burinskiy <52174539+ilya-burinskiy@users.noreply.github.com>
2026-02-18 16:21:30 +0300

    wip

--- Coro.hs
+++ Coro.hs
@@ -1,27 +1,31 @@
 import Control.Monad.Trans.Cont (runContT, ContT(..), callCC)
 import Control.Monad.Writer
+import Control.Monad.State
 
-coro1 :: ((ContT r (Writer String) ())
-          -> ContT r (Writer String) ())
-         -> ContT r (Writer String) (ContT r (Writer String) ())
+coro1 :: MonadWriter String m =>
+         (Maybe (ContT r m ()) -> ContT r m ())
+         -> ContT r m (Maybe (ContT r m ()))
 coro1 yield = do
     lift $ tell "1"
-    callCC $ \resume -> yield (resume ())
+    callCC $ \resume -> yield $ (Just $ resume ())
     lift $ tell "2"
-    return $ ContT $ \c -> c ()
+    return $ Nothing
 
-coro2 :: ((ContT r (Writer String) ())
-          -> ContT r (Writer String) ())
-         -> ContT r (Writer String) (ContT r (Writer String) ())
+coro2 :: MonadWriter String m =>
+         (Maybe (ContT r m ()) -> ContT r m ())
+         -> ContT r m (Maybe (ContT r m ()))
 coro2 yield = do
     lift $ tell "a"
+    callCC $ \resume -> yield (Just $ resume ())
     lift $ tell "b"
-    callCC $ \resume -> yield (resume ())
-    lift $ tell "c"
-    return $ ContT $ \c -> c ()
+    return $ Nothing
 
-runCoros = (execWriter . (`runContT` (\_ -> tell ""))) $ do
-    coro1Cont <- callCC coro1
-    coro2Cont <- callCC coro2
-    coro1Cont
-    coro2Cont
+runCoros = (execWriter . (`runContT` (\_ -> tell ""))) $
+    callCC coro1 >>= \mbCoro1Resume ->
+        case mbCoro1Resume of
+            Just coro1Resume ->
+                callCC coro2 >>= \mbCoro2Resume ->
+                    case mbCoro2Resume of
+                        Just coro2Resume -> coro1Resume >> coro2Resume
+                        Nothing -> return ()
+            Nothing -> return ()
